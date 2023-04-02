e04b9d768bfdd0e5b5e30aaa9dc11597b57fea22
JemilCodes <jemiluishaq277@gmail.com>
2023-04-02 11:25:45 +0100

    handle login message

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { store } from './app/store'
-import { Provider } from 'react-redux'
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
-if (process.env.NODE_ENV === 'production') disableReactDevTools()
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/*" element={<App />} />
+          <Route
+            path="/*"
+            element={
+              <>
+                <App />
+                <ToastContainer
+                  position="top-right"
+                  autoClose={5000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick={true}
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable={true}
+                  pauseOnHover
+                  theme="light"
+                />
+              </>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </Provider>
